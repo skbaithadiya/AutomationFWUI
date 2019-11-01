39b6bef47d32f1acb8e6982fbbfcdb0e8ec23ae9
Sunil Kumar Baithadiya <sunilkb@hitachiconsulting.com>
2019-11-01 14:38:31 +0900

    Initial commit

---
+++ ElementControls/WebElementControl.py
@@ -0,0 +1,3 @@
+# here we will define the webdriver, 
+# we will also define how to get ChromeDriver, Firefox Driver(GeckoDriver), Safari Driver etc.
+# Tester will call this class and pass the driver name, which one they wants
